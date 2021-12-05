8ae3d7619dba5baf1808ebea5182304ab18e7efc
dumon <dumono@gmail.com>
2021-12-05 22:42:11 +0300

    добавлено полнолуние

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup
 import settings
 from random import randint, choice
 from glob import glob
 from emoji import emojize
-import re
+import ephem
+from dateutil.parser import parse
 
 
 def get_smile(user_data):
@@ -17,8 +19,9 @@ def get_smile(user_data):
 def greet_user(update, context):
     context.user_data['emoji'] = get_smile(context.user_data)
     print('Вызван /start')
+    my_keyboard = ReplyKeyboardMarkup([['/cat']])
     update.message.reply_text("Привет, {}! Ты вызвал команду /start".format(update.message.from_user["username"]))
-    update.message.reply_text(f"ты можешь со мной поболтать {context.user_data['emoji']}!")
+    update.message.reply_text(f"ты можешь со мной поболтать {context.user_data['emoji']}!", reply_markup=my_keyboard)
     logging.info("connect username: {}".format(update.message.from_user["username"]))
     print("connect username: {}".format(update.message.from_user["username"]))
 
@@ -72,6 +75,58 @@ def wordcount(update, context):
     else:
         update.message.reply_text("Вы не ввели строку")
 
+#Вывод полнолуния
+def next_full_moon(update, context):
+    if len(context.args) != 1:
+        update.message.reply_text("Надо ввести дату")
+    else:
+        try:
+            param = parse(context.args[0])
+            update.message.reply_text(f"Следующее полнолуние:{ephem.next_full_moon(param)}")
+        except(ValueError):
+            update.message.reply_text("Вы ввели строку, не являющуюся датой")
+
+def calc(b, a = 0, math_operation="+"):
+    if math_operation == "+":
+        return a + b
+    elif math_operation == "-":
+        return a - b
+    elif math_operation == "*":
+        return a * b
+    else:
+        if b == 0:
+            return "Делить на 0 нельзя"
+        else:
+            a / b
+
+def is_number(number):
+    try:
+        number = float(number)
+    except ValueError:
+        return False
+    return True
+def is_operation(operation):
+    operations = "+-*/"
+    if operation in operations:
+        return True
+    else:
+        return False
+
+def calc_bot(update, context):
+    list_numbers = ['0']
+    list_operations = ['+']
+    for n in range(len(context.args)):
+        a = str(context.args[n]).replace(" ","")
+        if is_number(a):
+            list_numbers.append(float(a))
+        elif is_operation(a):
+            list_operations.append(a)
+    print(list_numbers)
+    print(list_operations)
+
+
+
+
 def main():
     mybot = Updater(settings.API_KEY)
 
@@ -80,6 +135,8 @@ def main():
     dp.add_handler(CommandHandler("guess", guess_number))
     dp.add_handler(CommandHandler("cat", send_cat_picture))
     dp.add_handler(CommandHandler("wordcount", wordcount))
+    dp.add_handler(CommandHandler("next_full_moon", next_full_moon))
+    dp.add_handler(CommandHandler("calc", calc_bot))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info("Бот стартовал")
