ab54a3ea049a88ebdf6b33ebe4e797be1b4fb1bc
dumon <dumono@gmail.com>
2021-12-06 23:05:46 +0300

    оптимизация калькулятора

--- main.py
+++ main.py
@@ -53,45 +53,46 @@ def is_number(number):
         return False
     return True
 
-def is_operation(operation):
-    if operation in OPERATIONS:
-        return True
-    else:
-        return False
-
 #калькулятор в боте
 def calc_bot(update, context):
     list_numbers = []
-    list_operations = ['+']
+    list_operations = ['+'] #для удобства и упрощения
     # рассматриваю пока два варианта - все пишется слитно или все пишется через пробел
     if len(context.args) != 1:
         #здесь просто - если через пробел - просто загоняем в нужные списки все аргументы
         for n in range(len(context.args)):
             a = str(context.args[n]).replace(" ","")
+            print(a)
             if is_number(a):
                 list_numbers.append(float(a))
-            elif is_operation(a):
+            elif a in OPERATIONS:
                 list_operations.append(a)
+            else:
+                #если нет последнего аргумента (т.е. после мат.операции ересь, то просто удаляем ее из списка
+                list_operations.pop(-1)
     else:
         # во втором случае бьем строку через разделители
         last_c = 0
         arg = context.args[0]
         for i in range(len(arg)):
             if arg[i] in OPERATIONS:
-                list_numbers.append(float(arg[last_c:i]))
+                num = arg[last_c:i]
+                if is_number(num):
+                    list_numbers.append(float(num))
                 list_operations.append(arg[i])
-                last_c = i+1
+                last_c = i + 1
         last_arg = arg[last_c:len(arg)]
         #я решил, что если не будет в конце второго операнда, то мы все равно посчитаем, но по правилам
         #ну или как вариант выкинем пользователю предупреждение
         # через ValueExeption специально не пошел, потому что можно вполне четко обойтись проверкой
         if is_number(last_arg):
             list_numbers.append(float(last_arg))
-        elif list_operations[len(list_operations)] == "+" or list_operations[len(list_operations)] =="-":
+        elif list_operations[-1] == "+" or list_operations[-1] == "-":
             list_numbers.append(0)
-        else:
+        elif list_operations[-1] == "*" or list_operations[-1] == "/":
             list_numbers.append(1)
-
+        else:
+            update.message.reply_text("Вы тут что-то совсем не то написали...")
     b = 0
     for index, operation in enumerate(list_operations):
         b = calc(b, list_numbers[index], operation)
